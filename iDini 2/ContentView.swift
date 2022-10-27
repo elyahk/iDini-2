371fbb7f69cad849affa84ee79db1c64b4f2e29c
Eldorbek Nusratov <eldorbek.nusratov.98@gmail.com>
2022-10-27 22:22:58 +0200

    Initial Commit

---
+++ iDini 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDini 2
+//
+//  Created by eldorbek nusratov on 10/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDini 2/iDini_2App.swift
@@ -0,0 +1,17 @@
+//
+//  iDini_2App.swift
+//  iDini 2
+//
+//  Created by eldorbek nusratov on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct iDini_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
